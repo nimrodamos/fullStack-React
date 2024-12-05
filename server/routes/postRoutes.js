93ab1ebbb94da0a9ee996ff9d13092ff53a6c865
nimrod amos <nimrodamos100@gmail.com>
2024-12-05 15:07:07 +0200

    new

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,21 +1,27 @@
-import express from 'express';
-import { getPosts, createPost, getPostById, updatePost, deletePost } from '../controllers/postController.js';
+import express from "express";
+import {
+  getPosts,
+  createPost,
+  getPostById,
+  updatePost,
+  deletePost,
+} from "../controllers/postController.js";
 
 const router = express.Router();
 
 // GET /posts - Get all posts
-router.get('/', getPosts);
+router.get("/", getPosts);
 
 // POST /posts - Create a new post
-router.post('/', createPost);
+router.post("/", createPost);
 
 // GET /posts/:postId - Get a specific post by its ID
-router.get('/:postId', getPostById);
+router.get("/:postId", getPostById);
 
 // PUT /posts/:postId - Update a post by its ID
-router.put('/:postId', updatePost);
+router.put("/:postId", updatePost);
 
 // DELETE /posts/:postId - Delete a post by its ID
-router.delete('/:postId', deletePost);
+router.delete("/:postId", deletePost);
 
 export default router;
